2655b2e767999d525e1c8b94deaa91b018c100e1
Cintia Pedroso <ndd_mpedroso@uolinc.com>
2021-03-15 20:29:57 -0300

    Adiciona cross origin

--- src/main/java/br/com/digitalmenu/resource/ProductResource.java
+++ src/main/java/br/com/digitalmenu/resource/ProductResource.java
@@ -6,6 +6,7 @@ import br.com.digitalmenu.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/product")
 public class ProductResource {
