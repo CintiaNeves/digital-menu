a610048f737d53db761334642d6289b8bab700dc
Cintia Pedroso <idd_mpedroso@uolinc.com>
2021-10-17 19:23:56 -0300

    DMD-002 - Cria mapper para customer

---
+++ src/main/java/br/com/digitalmenu/mapper/CustomerMapper.java
@@ -0,0 +1,33 @@
+package br.com.digitalmenu.mapper;
+
+import br.com.digitalmenu.domain.entity.Customer;
+import br.com.digitalmenu.domain.request.CustomerRequest;
+import br.com.digitalmenu.domain.response.CustomerResponse;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class CustomerMapper {
+
+    private final ModelMapper modelMapper;
+
+    public Customer toCustomer(CustomerRequest customerRequest) {
+        return modelMapper.map(customerRequest, Customer.class);
+    }
+
+    public CustomerResponse toCustomerResponse(Customer customer) {
+        return modelMapper.map(customer, CustomerResponse.class);
+    }
+
+    public List<CustomerResponse> toCustomerResponseList(List<Customer> customers){
+        return customers.stream()
+                .map(this::toCustomerResponse)
+                .collect(Collectors.toList());
+    }
+
+}
